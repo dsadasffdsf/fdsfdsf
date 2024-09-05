1900d4509c9a6e4c159895713504c123f0c588cf
dsadasffdsf <qwertfdsazxcvg122@mail.ru>
2024-09-05 18:35:50 +0300

    rdy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import packages
-const express = require("express");
+const express = require('express');
 const bodyParser = require('body-parser');
-const home = require("./routes/home");
+const home = require('./routes/home');
 const cors = require('cors');
 
 // Middlewares
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/home", home);
+app.use('/home', home);
 
 app.use(bodyParser.json());
 app.use(
@@ -20,24 +20,24 @@ app.use(
 );
 
 const users = [
-    { username: 'user1', password: 'password1', role: 'admin' },
-    { username: 'user2', password: 'password2', role: 'user' },
-    { username: 'user3', password: 'password3', role: 'moderator' },
-  ];
-
+  { username: 'user1', password: 'password1', role: 'admin' },
+  { username: 'user2', password: 'password2', role: 'user' },
+  { username: 'user3', password: 'password3', role: 'moderator' },
+];
 
 app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-  
-    const user = users.find((u) => u.username === username && u.password === password);
-  
-    if (user) {
-      res.status(200).json({ message: 'Login successful', user });
-    } else {
-      res.status(401).json({ message: 'Invalid credentials' });
-    }
-  });
+  const { username, password } = req.body;
 
+  const user = users.find((u) => u.username === username && u.password === password);
+
+  if (user) {
+    res
+      .status(200)
+      .json({ message: 'Login successful', data: { user: user.username, isAuth: true } });
+  } else {
+    res.status(401).json({ message: 'Неправильный логин или пароль', data: { isAuth: false } });
+  }
+});
 
 // connection
 const port = process.env.PORT || 9001;
