9efbfb530329bf154c50abb2e6b0f2e42e4ac3a7
dsadasffdsf <qwertfdsazxcvg122@mail.ru>
2024-09-05 13:20:23 +0300

    data

--- index.js
+++ index.js
@@ -9,6 +9,34 @@ app.use(express.json());
 // Routes
 app.use("/home", home);
 
+app.use(bodyParser.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: '*',
+  }),
+);
+
+const users = [
+    { username: 'user1', password: 'password1', role: 'admin' },
+    { username: 'user2', password: 'password2', role: 'user' },
+    { username: 'user3', password: 'password3', role: 'moderator' },
+  ];
+
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+  
+    const user = users.find((u) => u.username === username && u.password === password);
+  
+    if (user) {
+      res.status(200).json({ message: 'Login successful', user });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  });
+
+
 // connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
--- routes/home.js
+++ routes/home.js
@@ -8,4 +8,6 @@ router.get("/", async (req, res, next) => {
   });
 });
 
+
+
 module.exports = router;
